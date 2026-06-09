56d9e65edc1c4e7bf49277da6bc37de79db546b6
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 15:15:42 +0200

    Create 1860_2016_17.js

---
+++ teams/1860_2016_17.js
@@ -0,0 +1,51 @@
+// teams/1860_2016_17.js
+
+export const team_1860_2016_17 = {
+    id: "1860_2016_17",
+    name: "TSV 1860 München",
+    season: "2016/17",
+    category: "2. Bundesliga",
+    baseRating: 70,
+
+    players: [
+
+        { name: "Stefan Ortega", shortName: "Ortega", rating: 76, potential: 79, consistency: 76, positions: ["TW"] },
+        { name: "Jan Zimmermann", shortName: "Zimmermann", rating: 67, potential: 69, consistency: 66, positions: ["TW"] },
+        { name: "Maximilian Engl", shortName: "Engl", rating: 60, potential: 70, consistency: 54, positions: ["TW"] },
+
+        { name: "Felix Uduokhai", shortName: "Uduokhai", rating: 72, potential: 87, consistency: 66, positions: ["IV"] },
+        { name: "Abdoulaye Ba", shortName: "Ba", rating: 71, potential: 73, consistency: 70, positions: ["IV"] },
+        { name: "Milos Degenek", shortName: "Degenek", rating: 70, potential: 77, consistency: 67, positions: ["IV"] },
+        { name: "Jan Mauersberger", shortName: "Mauersberger", rating: 69, potential: 70, consistency: 70, positions: ["IV"] },
+        { name: "Rodnei", shortName: "Rodnei", rating: 68, potential: 69, consistency: 67, positions: ["IV"] },
+        { name: "Felix Weber", shortName: "Weber", rating: 66, potential: 71, consistency: 63, positions: ["IV"] },
+
+        { name: "Filip Stojkovic", shortName: "Stojkovic", rating: 71, potential: 75, consistency: 69, positions: ["RV"] },
+        { name: "Marnon Busch", shortName: "Busch", rating: 67, potential: 73, consistency: 63, positions: ["RV"] },
+
+        { name: "Maximilian Wittek", shortName: "Wittek", rating: 70, potential: 78, consistency: 66, positions: ["LV", "LM"] },
+        { name: "Sebastian Boenisch", shortName: "Boenisch", rating: 68, potential: 69, consistency: 67, positions: ["LV", "IV"] },
+        { name: "Kilian Jakob", shortName: "Jakob", rating: 63, potential: 76, consistency: 56, positions: ["LV", "LM"] },
+
+        { name: "Michael Liendl", shortName: "Liendl", rating: 75, potential: 76, consistency: 75, positions: ["ZOM", "ZM"] },
+        { name: "Florian Neuhaus", shortName: "Neuhaus", rating: 71, potential: 89, consistency: 63, positions: ["ZM", "ZOM"] },
+        { name: "Stefan Aigner", shortName: "Aigner", rating: 74, potential: 75, consistency: 73, positions: ["RM", "RF", "LM"] },
+        { name: "Daniel Adlung", shortName: "Adlung", rating: 70, potential: 71, consistency: 68, positions: ["ZM", "ZOM"] },
+        { name: "Goran Sukalo", shortName: "Sukalo", rating: 69, potential: 70, consistency: 70, positions: ["ZDM", "ZM"] },
+        { name: "Ilie Sanchez", shortName: "Ilie", rating: 72, potential: 76, consistency: 70, positions: ["ZDM", "ZM"] },
+        { name: "Frank Boya", shortName: "Boya", rating: 66, potential: 78, consistency: 58, positions: ["ZDM", "ZM"] },
+        { name: "Daylon Claasen", shortName: "Claasen", rating: 69, potential: 73, consistency: 66, positions: ["LM", "RM", "LF"] },
+        { name: "Lumor", shortName: "Lumor", rating: 67, potential: 79, consistency: 58, positions: ["LM", "LF", "LV"] },
+        { name: "Levent Aycicek", shortName: "Aycicek", rating: 66, potential: 72, consistency: 62, positions: ["ZOM", "RM"] },
+        { name: "Romuald Lacazette", shortName: "Lacazette", rating: 65, potential: 70, consistency: 61, positions: ["ZM"] },
+
+        { name: "Sascha Mölders", shortName: "Mölders", rating: 74, potential: 75, consistency: 74, positions: ["ST"] },
+        { name: "Ivica Olic", shortName: "Olic", rating: 71, potential: 72, consistency: 70, positions: ["ST", "LF"] },
+        { name: "Christian Gytkjaer", shortName: "Gytkjaer", rating: 73, potential: 76, consistency: 71, positions: ["ST"] },
+        { name: "Karim Matmour", shortName: "Matmour", rating: 70, potential: 71, consistency: 69, positions: ["RF", "LF", "ST"] },
+        { name: "Victor Andrade", shortName: "Victor", rating: 67, potential: 77, consistency: 59, positions: ["RF", "LF"] },
+        { name: "Ribamar", shortName: "Ribamar", rating: 65, potential: 80, consistency: 56, positions: ["ST"] },
+        { name: "Nico Karger", shortName: "Karger", rating: 65, potential: 70, consistency: 63, positions: ["ST", "RF"] },
+        { name: "Amilton", shortName: "Amilton", rating: 68, potential: 73, consistency: 65, positions: ["LF", "RF"] }
+    ]
+};
