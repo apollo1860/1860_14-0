461716d187249fa02c5d44e00564be915a341718
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 15:00:00 +0200

    Add TSV 1860 München team data for 2005/06 season

---
+++ teams/1860_2005_06.js
@@ -0,0 +1,43 @@
+// teams/1860_2005_06.js
+
+export const team_1860_2005_06 = {
+    id: "1860_2005_06",
+    name: "TSV 1860 München",
+    season: "2005/06",
+    category: "2. Bundesliga",
+    baseRating: 73,
+
+    players: [
+        { name: "Timo Ochs", shortName: "Ochs", rating: 76, potential: 79, consistency: 75, positions: ["TW"] },
+        { name: "Michael Hofmann", shortName: "Hofmann", rating: 73, potential: 74, consistency: 72, positions: ["TW"] },
+
+        { name: "Remo Meyer", shortName: "Meyer", rating: 76, potential: 78, consistency: 76, positions: ["IV", "RV"] },
+        { name: "Torben Hoffmann", shortName: "Hoffmann", rating: 75, potential: 76, consistency: 74, positions: ["IV"] },
+        { name: "Rodrigo Costa", shortName: "Rodrigo", rating: 74, potential: 75, consistency: 72, positions: ["IV"] },
+        { name: "Marcel Schäfer", shortName: "Schäfer", rating: 74, potential: 82, consistency: 72, positions: ["LV", "LM"] },
+        { name: "Lukasz Szukala", shortName: "Szukala", rating: 73, potential: 80, consistency: 70, positions: ["IV"] },
+        { name: "Quido Lanzaat", shortName: "Lanzaat", rating: 73, potential: 75, consistency: 71, positions: ["IV"] },
+        { name: "Fabian Lamotte", shortName: "Lamotte", rating: 71, potential: 74, consistency: 68, positions: ["IV", "RV"] },
+        { name: "Slobodan Komljenovic", shortName: "Komljenovic", rating: 71, potential: 73, consistency: 68, positions: ["IV", "ZDM"] },
+        { name: "Christoph Janker", shortName: "Janker", rating: 66, potential: 74, consistency: 61, positions: ["IV", "RV"] },
+
+        { name: "Daniel Baier", shortName: "Baier", rating: 77, potential: 82, consistency: 76, positions: ["ZDM", "ZM"] },
+        { name: "Shao Jiayi", shortName: "Shao", rating: 76, potential: 78, consistency: 74, positions: ["ZOM", "ZM", "LM"] },
+        { name: "Matthias Lehmann", shortName: "Lehmann", rating: 75, potential: 79, consistency: 74, positions: ["ZM", "ZDM"] },
+        { name: "Harald Cerny", shortName: "Cerny", rating: 74, potential: 75, consistency: 72, positions: ["RM", "RF"] },
+        { name: "Steffen Hofmann", shortName: "Hofmann", rating: 74, potential: 78, consistency: 72, positions: ["ZOM", "ZM"] },
+        { name: "Patrick Milchraum", shortName: "Milchraum", rating: 73, potential: 77, consistency: 70, positions: ["LM", "LV"] },
+        { name: "Marco Gebhardt", shortName: "Gebhardt", rating: 72, potential: 73, consistency: 70, positions: ["ZM", "RM"] },
+        { name: "Lance Davids", shortName: "Davids", rating: 70, potential: 75, consistency: 66, positions: ["ZDM", "ZM"] },
+        { name: "Emmanuel Krontiris", shortName: "Krontiris", rating: 69, potential: 73, consistency: 65, positions: ["RM", "RF", "ST"] },
+        { name: "Fabian Johnson", shortName: "Johnson", rating: 67, potential: 82, consistency: 60, positions: ["LM", "LV", "RM"] },
+        { name: "Nikolas Ledgerwood", shortName: "Ledgerwood", rating: 66, potential: 72, consistency: 61, positions: ["ZDM", "ZM"] },
+        { name: "Björn Ziegenbein", shortName: "Ziegenbein", rating: 66, potential: 73, consistency: 61, positions: ["RM", "RF"] },
+
+        { name: "Paul Agostino", shortName: "Agostino", rating: 77, potential: 78, consistency: 76, positions: ["ST"] },
+        { name: "Michal Kolomaznik", shortName: "Kolomaznik", rating: 75, potential: 76, consistency: 73, positions: ["ST"] },
+        { name: "Nemanja Vucicevic", shortName: "Vucicevic", rating: 74, potential: 77, consistency: 71, positions: ["ST", "RF", "LF"] },
+        { name: "Stefan Reisinger", shortName: "Reisinger", rating: 72, potential: 74, consistency: 69, positions: ["ST", "RF"] },
+        { name: "Nicky Adler", shortName: "Adler", rating: 68, potential: 76, consistency: 62, positions: ["ST"] }
+    ]
+};
