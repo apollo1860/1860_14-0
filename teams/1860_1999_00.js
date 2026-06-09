455071d22016f35a04848af870d8e70468423232
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 14:54:32 +0200

    Add TSV 1860 München team data for 1999/00 season

---
+++ teams/1860_1999_00.js
@@ -0,0 +1,197 @@
+// teams/1860_1999_00.js
+
+export const team_1860_1999_00 = {
+    id: "1860_1999_00",
+    name: "TSV 1860 München",
+    season: "1999/00",
+    category: "Bundesliga · Champions-League-Qualifikation",
+    baseRating: 84,
+
+    players: [
+        {
+            name: "Michael Hofmann",
+            shortName: "Hofmann",
+            rating: 82,
+            potential: 83,
+            consistency: 82,
+            positions: ["TW"]
+        },
+        {
+            name: "Simon Jentzsch",
+            shortName: "Jentzsch",
+            rating: 73,
+            potential: 81,
+            consistency: 68,
+            positions: ["TW"]
+        },
+        {
+            name: "Guido Gorges",
+            shortName: "Gorges",
+            rating: 67,
+            potential: 70,
+            consistency: 63,
+            positions: ["TW"]
+        },
+
+        {
+            name: "Thomas Miller",
+            shortName: "Miller",
+            rating: 81,
+            potential: 82,
+            consistency: 82,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Marco Kurz",
+            shortName: "Kurz",
+            rating: 82,
+            potential: 83,
+            consistency: 83,
+            positions: ["IV", "ZDM"]
+        },
+        {
+            name: "Ned Zelic",
+            shortName: "Zelic",
+            rating: 82,
+            potential: 84,
+            consistency: 79,
+            positions: ["IV", "ZDM"]
+        },
+        {
+            name: "Manfred Schwabl",
+            shortName: "Schwabl",
+            rating: 80,
+            potential: 81,
+            consistency: 80,
+            positions: ["RV", "RM", "ZM"]
+        },
+        {
+            name: "Daniel Borimirov",
+            shortName: "Borimirov",
+            rating: 80,
+            potential: 81,
+            consistency: 78,
+            positions: ["ZM", "RM", "ZOM"]
+        },
+        {
+            name: "Paul Agostino",
+            shortName: "Agostino",
+            rating: 80,
+            potential: 82,
+            consistency: 78,
+            positions: ["ST", "LF"]
+        },
+        {
+            name: "Harald Cerny",
+            shortName: "Cerny",
+            rating: 83,
+            potential: 84,
+            consistency: 84,
+            positions: ["RM", "RF"]
+        },
+        {
+            name: "Martin Max",
+            shortName: "Max",
+            rating: 86,
+            potential: 87,
+            consistency: 85,
+            positions: ["ST"]
+        },
+        {
+            name: "Abedi Pelé",
+            shortName: "Abedi Pelé",
+            rating: 84,
+            potential: 91,
+            consistency: 76,
+            positions: ["ZOM", "ZM", "LF"]
+        },
+        {
+            name: "Jens Jeremies",
+            shortName: "Jeremies",
+            rating: 84,
+            potential: 86,
+            consistency: 85,
+            positions: ["ZDM", "ZM"]
+        },
+        {
+            name: "Thomas Häßler",
+            shortName: "Häßler",
+            rating: 85,
+            potential: 90,
+            consistency: 82,
+            positions: ["ZOM", "ZM", "RM"]
+        },
+        {
+            name: "Bernhard Winkler",
+            shortName: "Winkler",
+            rating: 81,
+            potential: 83,
+            consistency: 80,
+            positions: ["ST", "RF"]
+        },
+        {
+            name: "Bernhard Trares",
+            shortName: "Trares",
+            rating: 79,
+            potential: 80,
+            consistency: 79,
+            positions: ["IV", "ZDM"]
+        },
+        {
+            name: "Horst Heldt",
+            shortName: "Heldt",
+            rating: 80,
+            potential: 82,
+            consistency: 78,
+            positions: ["ZM", "RM"]
+        },
+        {
+            name: "Holger Greilich",
+            shortName: "Greilich",
+            rating: 78,
+            potential: 80,
+            consistency: 76,
+            positions: ["LV", "IV"]
+        },
+        {
+            name: "Martin Stranzl",
+            shortName: "Stranzl",
+            rating: 75,
+            potential: 85,
+            consistency: 70,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Markus Schroth",
+            shortName: "Schroth",
+            rating: 75,
+            potential: 79,
+            consistency: 72,
+            positions: ["ST"]
+        },
+        {
+            name: "Davor Suker",
+            shortName: "Suker",
+            rating: 84,
+            potential: 91,
+            consistency: 78,
+            positions: ["ST"]
+        },
+        {
+            name: "Steffen Hofmann",
+            shortName: "Hofmann",
+            rating: 70,
+            potential: 80,
+            consistency: 65,
+            positions: ["ZOM", "ZM"]
+        },
+        {
+            name: "Necat Aygün",
+            shortName: "Aygün",
+            rating: 68,
+            potential: 77,
+            consistency: 62,
+            positions: ["IV"]
+        }
+    ]
+};
