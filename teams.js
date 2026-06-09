5f434f66c05d4504976cb1274ca2c1727557863f
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 14:06:21 +0200

    Add TEAM_POOL with historical team imports

---
+++ teams.js
@@ -0,0 +1,23 @@
+import { team_1860_1965_66 } from "./teams/1860_1965_66.js";
+import { team_1860_1976_77 } from "./teams/1860_1976_77.js";
+import { team_1860_1981_82 } from "./teams/1860_1981_82.js";
+import { team_1860_1993_94 } from "./teams/1860_1993_94.js";
+import { team_1860_1999_00 } from "./teams/1860_1999_00.js";
+import { team_1860_2005_06 } from "./teams/1860_2005_06.js";
+import { team_1860_2010_11 } from "./teams/1860_2010_11.js";
+import { team_1860_2016_17 } from "./teams/1860_2016_17.js";
+import { team_1860_2017_18 } from "./teams/1860_2017_18.js";
+import { team_1860_2022_23 } from "./teams/1860_2022_23.js";
+
+export const TEAM_POOL = [
+    team_1860_1965_66,
+    team_1860_1976_77,
+    team_1860_1981_82,
+    team_1860_1993_94,
+    team_1860_1999_00,
+    team_1860_2005_06,
+    team_1860_2010_11,
+    team_1860_2016_17,
+    team_1860_2017_18,
+    team_1860_2022_23
+];
