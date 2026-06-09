4ffb74ae3580e440c26f1b247784a56e799898b2
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 14:50:06 +0200

    Create 1860_1993_94.js

---
+++ teams/1860_1993_94.js
@@ -0,0 +1,191 @@
+// teams/1860_1993_94.js
+
+export const team_1860_1993_94 = {
+    id: "1860_1993_94",
+    name: "TSV 1860 München",
+    season: "1993/94",
+    category: "Aufstiegsteam 2. Bundesliga",
+    baseRating: 76,
+
+    players: [
+        {
+            name: "Michael Hofmann",
+            shortName: "Hofmann",
+            rating: 77,
+            potential: 80,
+            consistency: 76,
+            positions: ["TW"]
+        },
+        {
+            name: "Gerald Hillringhaus",
+            shortName: "Hillringhaus",
+            rating: 70,
+            potential: 72,
+            consistency: 67,
+            positions: ["TW"]
+        },
+
+        {
+            name: "Jens Keller",
+            shortName: "Keller",
+            rating: 79,
+            potential: 82,
+            consistency: 77,
+            positions: ["IV"]
+        },
+        {
+            name: "Thomas Miller",
+            shortName: "Miller",
+            rating: 78,
+            potential: 79,
+            consistency: 78,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Bernhard Trares",
+            shortName: "Trares",
+            rating: 77,
+            potential: 78,
+            consistency: 76,
+            positions: ["IV", "ZDM"]
+        },
+        {
+            name: "Reiner Maurer",
+            shortName: "Maurer",
+            rating: 76,
+            potential: 76,
+            consistency: 78,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Jochen Kientz",
+            shortName: "Kientz",
+            rating: 75,
+            potential: 78,
+            consistency: 73,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Jürgen Schnell",
+            shortName: "Schnell",
+            rating: 74,
+            potential: 75,
+            consistency: 72,
+            positions: ["LV", "IV"]
+        },
+        {
+            name: "Andreas Hartig",
+            shortName: "Hartig",
+            rating: 71,
+            potential: 73,
+            consistency: 68,
+            positions: ["RV", "IV"]
+        },
+        {
+            name: "Lutz Braun",
+            shortName: "Braun",
+            rating: 70,
+            potential: 72,
+            consistency: 67,
+            positions: ["IV"]
+        },
+
+        {
+            name: "Armin Störzenhofecker",
+            shortName: "Störzenhofecker",
+            rating: 78,
+            potential: 79,
+            consistency: 78,
+            positions: ["ZM", "ZDM"]
+        },
+        {
+            name: "Roland Kneißl",
+            shortName: "Kneißl",
+            rating: 77,
+            potential: 77,
+            consistency: 76,
+            positions: ["ZOM", "ZM", "ST"]
+        },
+        {
+            name: "Thomas Seeliger",
+            shortName: "Seeliger",
+            rating: 76,
+            potential: 77,
+            consistency: 74,
+            positions: ["LM", "RM", "ZOM"]
+        },
+        {
+            name: "Niels Schlotterbeck",
+            shortName: "Schlotterbeck",
+            rating: 74,
+            potential: 76,
+            consistency: 72,
+            positions: ["ZM", "ZDM"]
+        },
+        {
+            name: "Peter Zeiler",
+            shortName: "Zeiler",
+            rating: 73,
+            potential: 74,
+            consistency: 71,
+            positions: ["ZM", "RM"]
+        },
+
+        {
+            name: "Peter Pacult",
+            shortName: "Pacult",
+            rating: 81,
+            potential: 82,
+            consistency: 80,
+            positions: ["ST"]
+        },
+        {
+            name: "Manfred Schwabl",
+            shortName: "Schwabl",
+            rating: 79,
+            potential: 80,
+            consistency: 78,
+            positions: ["ZM", "RM", "RV"]
+        },
+        {
+            name: "Bernhard Winkler",
+            shortName: "Winkler",
+            rating: 78,
+            potential: 82,
+            consistency: 75,
+            positions: ["ST", "RF"]
+        },
+        {
+            name: "Piotr Nowak",
+            shortName: "Nowak",
+            rating: 78,
+            potential: 82,
+            consistency: 74,
+            positions: ["ZOM", "ZM"]
+        },
+        {
+            name: "Manni Bender",
+            shortName: "Bender",
+            rating: 76,
+            potential: 77,
+            consistency: 75,
+            positions: ["RF", "RM", "ST"]
+        },
+        {
+            name: "Martin Max",
+            shortName: "Max",
+            rating: 75,
+            potential: 86,
+            consistency: 72,
+            positions: ["ST"]
+        },
+        {
+            name: "Bernhard Weis",
+            shortName: "Weis",
+            rating: 72,
+            potential: 74,
+            consistency: 69,
+            positions: ["ST", "LF"]
+        }
+    ]
+};
