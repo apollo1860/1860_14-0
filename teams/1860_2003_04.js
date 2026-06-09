1acf37c649708d453f0adb7e0997c61fc1ac49db
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 15:42:51 +0200

    Create 1860_2003_04.js

---
+++ teams/1860_2003_04.js
@@ -0,0 +1,45 @@
+// teams/1860_2003_04.js
+
+export const team_1860_2003_04 = {
+    id: "1860_2003_04",
+    name: "TSV 1860 München",
+    season: "2003/04",
+    category: "Bundesliga",
+    baseRating: 77,
+
+    players: [
+        { name: "Michael Hofmann", shortName: "Hofmann", rating: 81, potential: 82, consistency: 81, positions: ["TW"] },
+        { name: "André Lenz", shortName: "Lenz", rating: 74, potential: 75, consistency: 73, positions: ["TW"] },
+        { name: "Matthias Küfner", shortName: "Küfner", rating: 68, potential: 71, consistency: 65, positions: ["TW"] },
+        { name: "Christian Horn", shortName: "Horn", rating: 62, potential: 68, consistency: 56, positions: ["TW"] },
+
+        { name: "Martin Stranzl", shortName: "Stranzl", rating: 81, potential: 85, consistency: 78, positions: ["IV"] },
+        { name: "Marco Kurz", shortName: "Kurz", rating: 80, potential: 80, consistency: 81, positions: ["IV"] },
+        { name: "Remo Meyer", shortName: "Meyer", rating: 78, potential: 80, consistency: 77, positions: ["IV", "RV"] },
+        { name: "Rodrigo Costa", shortName: "Costa", rating: 78, potential: 80, consistency: 76, positions: ["IV", "LV"] },
+        { name: "Janne Saarinen", shortName: "Saarinen", rating: 76, potential: 78, consistency: 75, positions: ["LV", "LM"] },
+        { name: "Andreas Görlitz", shortName: "Görlitz", rating: 79, potential: 86, consistency: 74, positions: ["RV", "RM"] },
+        { name: "Fernando", shortName: "Fernando", rating: 72, potential: 76, consistency: 68, positions: ["IV"] },
+        { name: "Joe Kendrick", shortName: "Kendrick", rating: 69, potential: 74, consistency: 64, positions: ["LV", "RV"] },
+        { name: "Christoph Janker", shortName: "Janker", rating: 62, potential: 72, consistency: 55, positions: ["IV", "RV"] },
+
+        { name: "Harald Cerny", shortName: "Cerny", rating: 81, potential: 82, consistency: 81, positions: ["RM", "RF"] },
+        { name: "Horst Heldt", shortName: "Heldt", rating: 79, potential: 80, consistency: 78, positions: ["ZM", "RM"] },
+        { name: "Daniel Borimirov", shortName: "Borimirov", rating: 78, potential: 80, consistency: 76, positions: ["ZM", "ZOM"] },
+        { name: "Thomas Häßler", shortName: "Häßler", rating: 80, potential: 90, consistency: 74, positions: ["ZOM", "ZM"] },
+        { name: "Gerhard Poschner", shortName: "Poschner", rating: 76, potential: 77, consistency: 75, positions: ["ZDM", "ZM"] },
+        { name: "Danny Schwarz", shortName: "Schwarz", rating: 75, potential: 76, consistency: 75, positions: ["ZM", "ZDM"] },
+        { name: "Matthias Lehmann", shortName: "Lehmann", rating: 72, potential: 78, consistency: 68, positions: ["ZM", "ZDM"] },
+        { name: "Daniel Baier", shortName: "Baier", rating: 70, potential: 82, consistency: 64, positions: ["ZM", "ZDM"] },
+        { name: "Miloš Marić", shortName: "Marić", rating: 73, potential: 78, consistency: 68, positions: ["ZOM", "ZM"] },
+        { name: "Stefan Schröter", shortName: "Schröter", rating: 77, potential: 78, consistency: 75, positions: ["LM", "RM"] },
+        { name: "Christophe Lepoint", shortName: "Lepoint", rating: 61, potential: 72, consistency: 54, positions: ["ZM", "ZDM"] },
+
+        { name: "Benjamin Lauth", shortName: "Lauth", rating: 83, potential: 87, consistency: 81, positions: ["ST"] },
+        { name: "Paul Agostino", shortName: "Agostino", rating: 79, potential: 80, consistency: 78, positions: ["ST", "LF"] },
+        { name: "Markus Schroth", shortName: "Schroth", rating: 77, potential: 79, consistency: 75, positions: ["ST"] },
+        { name: "Francis Kioyo", shortName: "Kioyo", rating: 74, potential: 77, consistency: 71, positions: ["ST"] },
+        { name: "Lance Davids", shortName: "Davids", rating: 70, potential: 78, consistency: 63, positions: ["RF", "RM", "ST"] },
+        { name: "Marcus Pürk", shortName: "Pürk", rating: 71, potential: 72, consistency: 68, positions: ["ST", "RF"] }
+    ]
+};
