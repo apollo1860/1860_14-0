2201acbd51351fa5b79e4feea009ecb8aac32f29
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 14:39:30 +0200

    Create 1860_1965_66.js

---
+++ teams/1860_1965_66.js
@@ -0,0 +1,183 @@
+// teams/1860_1965_66.js
+
+export const team_1860_1965_66 = {
+    id: "1860_1965_66",
+    name: "TSV 1860 München",
+    season: "1965/66",
+    category: "Meisterteam",
+    baseRating: 84,
+
+    players: [
+        {
+            name: "Petar Radenković",
+            shortName: "Radenković",
+            rating: 86,
+            potential: 87,
+            consistency: 84,
+            positions: ["TW"]
+        },
+        {
+            name: "Wilfried Tepe",
+            shortName: "Tepe",
+            rating: 68,
+            potential: 70,
+            consistency: 62,
+            positions: ["TW"]
+        },
+
+        {
+            name: "Bernd Patzke",
+            shortName: "Patzke",
+            rating: 83,
+            potential: 84,
+            consistency: 82,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Hans Reich",
+            shortName: "Reich",
+            rating: 82,
+            potential: 83,
+            consistency: 81,
+            positions: ["IV"]
+        },
+        {
+            name: "Manfred Wagner",
+            shortName: "Wagner",
+            rating: 81,
+            potential: 82,
+            consistency: 80,
+            positions: ["IV"]
+        },
+        {
+            name: "Rudolf Zeiser",
+            shortName: "Zeiser",
+            rating: 76,
+            potential: 77,
+            consistency: 74,
+            positions: ["LV", "IV"]
+        },
+        {
+            name: "Rudolf Steiner",
+            shortName: "Steiner",
+            rating: 75,
+            potential: 76,
+            consistency: 73,
+            positions: ["IV", "RV"]
+        },
+        {
+            name: "Alfred Kohlhäufl",
+            shortName: "Kohlhäufl",
+            rating: 66,
+            potential: 72,
+            consistency: 60,
+            positions: ["IV"]
+        },
+
+        {
+            name: "Željko Perušić",
+            shortName: "Perušić",
+            rating: 86,
+            potential: 87,
+            consistency: 88,
+            positions: ["ZDM", "ZM", "IV"]
+        },
+        {
+            name: "Peter Grosser",
+            shortName: "Grosser",
+            rating: 88,
+            potential: 88,
+            consistency: 86,
+            positions: ["ZOM", "ZM", "ST"]
+        },
+        {
+            name: "Otto Luttrop",
+            shortName: "Luttrop",
+            rating: 80,
+            potential: 81,
+            consistency: 78,
+            positions: ["ZM", "ZDM"]
+        },
+        {
+            name: "Hans Küppers",
+            shortName: "Küppers",
+            rating: 81,
+            potential: 82,
+            consistency: 77,
+            positions: ["ZM", "ZOM"]
+        },
+
+        {
+            name: "Friedhelm Konietzka",
+            shortName: "Konietzka",
+            rating: 89,
+            potential: 89,
+            consistency: 87,
+            positions: ["ST"]
+        },
+        {
+            name: "Rudi Brunnenmeier",
+            shortName: "Brunnenmeier",
+            rating: 86,
+            potential: 87,
+            consistency: 84,
+            positions: ["ST", "LF"]
+        },
+        {
+            name: "Alfred Heiß",
+            shortName: "Heiß",
+            rating: 82,
+            potential: 83,
+            consistency: 80,
+            positions: ["RF", "ST", "LF"]
+        },
+        {
+            name: "Wilfried Kohlars",
+            shortName: "Kohlars",
+            rating: 78,
+            potential: 79,
+            consistency: 75,
+            positions: ["LF", "RF", "ST"]
+        },
+        {
+            name: "Hans Rebele",
+            shortName: "Rebele",
+            rating: 77,
+            potential: 78,
+            consistency: 74,
+            positions: ["LF", "ST"]
+        },
+        {
+            name: "Ludwig Bründl",
+            shortName: "Bründl",
+            rating: 73,
+            potential: 81,
+            consistency: 68,
+            positions: ["ST", "RF"]
+        },
+        {
+            name: "Helmut Richert",
+            shortName: "Richert",
+            rating: 70,
+            potential: 72,
+            consistency: 66,
+            positions: ["ST", "RF"]
+        },
+        {
+            name: "Hans Fischer",
+            shortName: "Fischer",
+            rating: 69,
+            potential: 72,
+            consistency: 65,
+            positions: ["LF", "RF"]
+        },
+        {
+            name: "Ernst Winterhalder",
+            shortName: "Winterhalder",
+            rating: 68,
+            potential: 71,
+            consistency: 64,
+            positions: ["ST", "LF"]
+        }
+    ]
+};
