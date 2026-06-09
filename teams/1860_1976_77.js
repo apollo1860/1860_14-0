526adfe91c1f7183565ff6b9a51a7f037e81e977
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 14:46:22 +0200

    Add team data for TSV 1860 München 1976/77 season

---
+++ teams/1860_1976_77.js
@@ -0,0 +1,167 @@
+// teams/1860_1976_77.js
+
+export const team_1860_1976_77 = {
+    id: "1860_1976_77",
+    name: "TSV 1860 München",
+    season: "1976/77",
+    category: "Aufstiegsteam 2. Bundesliga Süd",
+    baseRating: 75,
+
+    players: [
+        {
+            name: "Bernhard Hartmann",
+            shortName: "Hartmann",
+            rating: 78,
+            potential: 79,
+            consistency: 77,
+            positions: ["TW"]
+        },
+        {
+            name: "Manfred Eiben",
+            shortName: "Eiben",
+            rating: 68,
+            potential: 70,
+            consistency: 64,
+            positions: ["TW"]
+        },
+
+        {
+            name: "Alfred Kohlhäufl",
+            shortName: "Kohlhäufl",
+            rating: 78,
+            potential: 79,
+            consistency: 78,
+            positions: ["IV"]
+        },
+        {
+            name: "Ahmet Glavović",
+            shortName: "Glavović",
+            rating: 78,
+            potential: 79,
+            consistency: 77,
+            positions: ["IV"]
+        },
+        {
+            name: "Willi Bierofka",
+            shortName: "Bierofka",
+            rating: 77,
+            potential: 78,
+            consistency: 76,
+            positions: ["LV", "IV"]
+        },
+        {
+            name: "Dieter Agatha",
+            shortName: "Agatha",
+            rating: 74,
+            potential: 75,
+            consistency: 72,
+            positions: ["RV", "IV"]
+        },
+        {
+            name: "Richard Mamajewski",
+            shortName: "Mamajewski",
+            rating: 71,
+            potential: 72,
+            consistency: 68,
+            positions: ["IV"]
+        },
+
+        {
+            name: "Jimmy Hartwig",
+            shortName: "Hartwig",
+            rating: 81,
+            potential: 84,
+            consistency: 79,
+            positions: ["ZDM", "ZM"]
+        },
+        {
+            name: "Hans Haunstein",
+            shortName: "Haunstein",
+            rating: 76,
+            potential: 77,
+            consistency: 75,
+            positions: ["ZM"]
+        },
+        {
+            name: "Jan-Hoiland Nielsen",
+            shortName: "Nielsen",
+            rating: 76,
+            potential: 77,
+            consistency: 74,
+            positions: ["ZM", "ZDM"]
+        },
+        {
+            name: "Alfred Herberth",
+            shortName: "Herberth",
+            rating: 75,
+            potential: 76,
+            consistency: 73,
+            positions: ["ZM"]
+        },
+        {
+            name: "Fritz Bischoff",
+            shortName: "Bischoff",
+            rating: 74,
+            potential: 75,
+            consistency: 72,
+            positions: ["ZM"]
+        },
+        {
+            name: "Karl-Heinz Pelka",
+            shortName: "Pelka",
+            rating: 68,
+            potential: 70,
+            consistency: 64,
+            positions: ["ZM"]
+        },
+
+        {
+            name: "Peter Falter",
+            shortName: "Falter",
+            rating: 80,
+            potential: 81,
+            consistency: 78,
+            positions: ["ST"]
+        },
+        {
+            name: "Georg Metzger",
+            shortName: "Metzger",
+            rating: 79,
+            potential: 80,
+            consistency: 77,
+            positions: ["ST", "ZOM"]
+        },
+        {
+            name: "Anton Nachreiner",
+            shortName: "Nachreiner",
+            rating: 77,
+            potential: 78,
+            consistency: 75,
+            positions: ["ST"]
+        },
+        {
+            name: "Alfred Rieß",
+            shortName: "Rieß",
+            rating: 73,
+            potential: 74,
+            consistency: 70,
+            positions: ["ST", "RF"]
+        },
+        {
+            name: "Heinz Tochtermann",
+            shortName: "Tochtermann",
+            rating: 72,
+            potential: 73,
+            consistency: 69,
+            positions: ["ST"]
+        },
+        {
+            name: "Peter Scheifler",
+            shortName: "Scheifler",
+            rating: 71,
+            potential: 72,
+            consistency: 68,
+            positions: ["ST"]
+        }
+    ]
+};
