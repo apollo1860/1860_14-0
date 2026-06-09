482c6d3c494d963a19bb44307e3036de884667cf
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2026-06-09 15:28:28 +0200

    Update teams.js

--- teams.js
+++ teams.js
@@ -6,6 +6,8 @@ import { team_1860_1999_00 } from "./teams/1860_1999_00.js";
 import { team_1860_2005_06 } from "./teams/1860_2005_06.js";
 import { team_1860_2010_11 } from "./teams/1860_2010_11.js";
 import { team_1860_2016_17 } from "./teams/1860_2016_17.js";
+import { team_1860_2003_04 } from "./teams/1860_2003_04.js";
+import { team_1860_2014_15 } from "./teams/1860_2014_15.js";
 import { team_1860_2017_18 } from "./teams/1860_2017_18.js";
 import { team_1860_2022_23 } from "./teams/1860_2022_23.js";
 
@@ -15,8 +17,10 @@ export const TEAM_POOL = [
     team_1860_1981_82,
     team_1860_1993_94,
     team_1860_1999_00,
+    team_1860_2003_04,
     team_1860_2005_06,
     team_1860_2010_11,
+    team_1860_2014_15,
     team_1860_2016_17,
     team_1860_2017_18,
     team_1860_2022_23
